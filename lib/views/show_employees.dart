f6a4fc9194825c7cbb2a7f97eb890eeae9654849
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-08-04 08:12:29 +0300

    refactor

--- lib/views/show_employees.dart
+++ lib/views/show_employees.dart
@@ -31,61 +31,63 @@ class _ShowEmployess extends State<ShowEmployees> {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            BlocBuilder<EmployeeBloc, EmployeeState>(builder: (context, state) {
-              if (state is EmployeeLoadedState) {
-                return Expanded(
-                  child: ListView.builder(
-                      itemCount: state.employees.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 20),
-                          child: Card(
-                            elevation: 5.0,
-                            margin: const EdgeInsets.all(8.0),
-                            color: Colors.white,
-                            shadowColor: Colors.indigo,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5.0),
+      body: buildEmployeeList(),
+    );
+  }
+
+  Center buildEmployeeList() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          BlocBuilder<EmployeeBloc, EmployeeState>(builder: (context, state) {
+            if (state is EmployeeLoadedState) {
+              return Expanded(
+                child: ListView.builder(
+                    itemCount: state.employees.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        child: buildEmployeeCard(state, index),
+                      );
+                    }),
+              );
+            } else if (state is EmployeeErrorState) {
+              return Text('Error: ${state.errorMessage}');
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          }),
+        ],
+      ),
+    );
+  }
+
+  Card buildEmployeeCard(EmployeeLoadedState state, int index) {
+    return Card(
+                          elevation: 5.0,
+                          margin: const EdgeInsets.all(8.0),
+                          color: Colors.white,
+                          shadowColor: Colors.indigo,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          child: ListTile(
+                            title: Text(
+                              "${state.employees[index].name?.first ?? 'No Name'} ${state.employees[index].name?.last ?? 'No Name'}",
+                              style: const TextStyle(fontWeight: FontWeight.w700),
                             ),
-                            child: ListTile(
-                              title: Text(
-                                "${state.employees[index].name?.first ?? 'No Name'} ${state.employees[index].name?.last ?? 'No Name'}",
-                                style: const TextStyle(fontWeight: FontWeight.w700),
-                              ),
-                              subtitle: Text(
-                                state.employees[index].email.toString(),
-                                style: const TextStyle(color: Colors.indigo),
-                              ),
-                              trailing: Text(
-                                state.employees[index].address?.city ??
-                                    'No Adress',
-                                style: const TextStyle(fontWeight: FontWeight.w500),
-                              ),
+                            subtitle: Text(
+                              state.employees[index].email.toString(),
+                              style: const TextStyle(color: Colors.indigo),
+                            ),
+                            trailing: Text(
+                              state.employees[index].address?.city ??
+                                  'No Adress',
+                              style: const TextStyle(fontWeight: FontWeight.w500),
                             ),
                           ),
                         );
-                      }),
-                );
-              } else if (state is EmployeeErrorState) {
-                return Text('Error: ${state.errorMessage}');
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            }),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.indigo,
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add,color: Colors.white,),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
   }
 }
